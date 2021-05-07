d94c421f21b1fb386cdc21e6585735c58815e5eb
Yuichi Fujishige <nakiwo@gmail.com>
2021-05-07 20:45:52 +0900

    add SampleApp

---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by 雄一 on 2021/05/07.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
